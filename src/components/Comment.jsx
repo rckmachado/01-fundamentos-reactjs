85e8a25a231db6a946215e37a349ba0b6f8d287a
Rick Machado <contato.rickmachado@gmail.com>
2022-11-29 10:46:41 -0300

    OK!

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 
 export function Comment() {
     return (
-        <div></div>
+        <div className={styles.comment}>
+            <img src="https://github.com/rckmachado.png" alt=""/>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndtime}>
+                            <strong>Rick Machado</strong>
+                            <time title="14 de Julho às 18:30" dateTime="2022-07-14">Cerca de 1h atrás</time>
+                        </div>
+
+                    <button title="Deletar comentário">
+                        <Trash size={20} />
+                    </button>
+                    </header>
+
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </div>
+            </div>
+
+            <footer>
+                Aplaudir
+            </footer>
+        </div>
     )
 }
