a0da72fb26419ecc5d30a00e06bd995e56c710ab
Rick Machado <contato.rickmachado@gmail.com>
2022-11-29 10:46:35 -0300

    Ok!

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post() {
@@ -41,7 +42,10 @@ export function Post() {
                     <button type="submit">Comentar</button>
                 </footer>
             </form>
-        </article>
 
+            <div className={styles.commentList}>
+                <Comment /><Comment /><Comment />
+            </div>
+        </article>
     )
 }
